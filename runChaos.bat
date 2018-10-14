a310a55517f468b89dc94df0934a1a709b7459a5
Juan Antonio Brena Moral <bren@juanantonio.info>
2018-10-14 18:25:58 +0200

    fixed issue with external jar

--- runChaos.bat
+++ runChaos.bat
@@ -1,2 +1,2 @@
 mvn clean package
-java -Xbootclasspath/a:aspectjrt-1.9.1.jar -cp ./target/spring-boot-chaos-poc-0.1.0-SNAPSHOT.jar -Dloader.path=chaos-monkey-spring-boot-2.0.0.jar org.springframework.boot.loader.PropertiesLauncher --spring.profiles.active=chaos-monkey
+java -cp ./target/spring-boot-chaos-poc-0.1.0-SNAPSHOT.jar -Dloader.path=chaos-monkey-spring-boot-2.0.1-SNAPSHOT-jar-with-dependencies.jar org.springframework.boot.loader.PropertiesLauncher --spring.profiles.active=chaos-monkey
