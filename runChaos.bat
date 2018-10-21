7b9779c4b2f8394403f2643e2a3049198b04a765
Juan Antonio Breña Moral <bren@juanantonio.info>
2018-10-21 16:01:39 +0200

    Minor change

--- runChaos.bat
+++ runChaos.bat
@@ -1,2 +1,2 @@
 mvn clean package
-java -cp ./target/spring-boot-chaos-poc-0.1.0-SNAPSHOT.jar -Dloader.path=-Dloader.path=chaos-monkey-spring-boot-2.0.1-SNAPSHOT-jar-with-dependencies.jar org.springframework.boot.loader.PropertiesLauncher --spring.profiles.active=chaos-monkey
+java -cp ./target/spring-boot-chaos-poc-0.1.0-SNAPSHOT.jar -Dloader.path=chaos-monkey-spring-boot-2.0.1-SNAPSHOT-jar-with-dependencies.jar org.springframework.boot.loader.PropertiesLauncher --spring.profiles.active=chaos-monkey
